c85e2560f48a58afe077763b951705e73b5447e0
UET-Boy <spellbreaker907@gmail.com>
2020-08-03 12:11:55 +0500

    new work started

---
+++ proj1/target/debug/deps/proj1-0f890beb302bfac5.d
@@ -0,0 +1,5 @@
+/home/redskull/Downloads/RustProjects/proj1/target/debug/deps/proj1-0f890beb302bfac5: src/main.rs
+
+/home/redskull/Downloads/RustProjects/proj1/target/debug/deps/proj1-0f890beb302bfac5.d: src/main.rs
+
+src/main.rs:
---
+++ proj1/target/debug/proj1.d
@@ -0,0 +1,1 @@
+/home/redskull/Downloads/RustProjects/proj1/target/debug/proj1: /home/redskull/Downloads/RustProjects/proj1/src/main.rs
