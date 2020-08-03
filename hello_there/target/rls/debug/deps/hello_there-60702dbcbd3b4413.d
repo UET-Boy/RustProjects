e211a15835d61a7803beaf82f9415c9d5a280110
UET-Boy <spellbreaker907@gmail.com>
2020-08-03 12:09:37 +0500

    please now

---
+++ hello_there/target/rls/debug/deps/hello_there-60702dbcbd3b4413.d
@@ -0,0 +1,5 @@
+/home/redskull/Downloads/RustProjects/hello_there/target/rls/debug/deps/hello_there-60702dbcbd3b4413.rmeta: src/main.rs
+
+/home/redskull/Downloads/RustProjects/hello_there/target/rls/debug/deps/hello_there-60702dbcbd3b4413.d: src/main.rs
+
+src/main.rs:
---
+++ hello_there/target/rls/debug/deps/hello_there-c825be0c86a432e0.d
@@ -0,0 +1,5 @@
+/home/redskull/Downloads/RustProjects/hello_there/target/rls/debug/deps/hello_there-c825be0c86a432e0.rmeta: src/main.rs
+
+/home/redskull/Downloads/RustProjects/hello_there/target/rls/debug/deps/hello_there-c825be0c86a432e0.d: src/main.rs
+
+src/main.rs:
